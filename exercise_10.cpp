26d544e42adc17d39cf6823d83b33985169ee2e4
Arda <arderd@outlook.com>
2024-04-14 03:33:06 +0300

    exercise_10: r-value and l-value binding are done

--- exercise_10.cpp
+++ exercise_10.cpp
@@ -7,6 +7,12 @@
 
 using namespace std;
 
+
+// Why does this bind only to l-values? The signature int& a means that foo takes an argument by non-const reference.
+// Non-const references can only bind to modifiable l-values because:
+// They need an existing object to refer to (something with a stable address).
+// They potentially modify the object, so the referred object must be non-const.
+
 void foo(int& a) { // this only binds to l-value
     a++;
     cout << "l-value is: " << a << endl;
@@ -17,9 +23,22 @@ void foo(int& a) { // this only binds to l-value
 //    cout << "Beccause of Clone l-value and r-value are: " << a << endl;
 //}
 
+
+// Why does this bind only to r-values? The signature int&& a means foo takes an argument by r-value reference.
+// R-value references are designed to bind specifically to temporaries:
+// They enable the function to operate on a temporary without creating a copy.
+// They are often used for move semantics, which can significantly optimize performance by "moving" resources (like dynamically allocated memory)
+// from temporaries to new owners.
+
 void foo(int&& a) { // this only binds to r-value
     cout << "r-value is: " << a << endl;
 }
+
+
+// What does const reference mean here? When you use a const reference (const int& a), the function foo can accept both l-values and r-values:
+// For l-values: It can bind to l-values (like variables) without risk of modification, hence preserving the constancy of user-defined variables.
+// For r-values: It can also bind to r-values. Since the function does not modify the argument (guaranteed by the const qualifier), it's safe to bind to temporaries.
+// This is useful because it prevents unnecessary copying of data.
 
 void foo(const int& a) { // this binds to both l-value and r-value with ref
     cout << "const value is: " << a << endl;

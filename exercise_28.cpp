b6f2901e7de7dda5b98b127a9f87e3462f6bb7e2
Arda <arderd@outlook.com>
2024-05-31 16:34:53 +0300

    exercise_30 is done

---
+++ exercise_28.cpp
@@ -0,0 +1,19 @@
+// std::optional<T>
+// = default, = delete, std::declval
+// [[nodiscard]], std::unique_ptr, std::shared_ptr
+// duck typing? type erasure: std::function
+// SFINAE: enable_if, and its relation with concepts
+// ADL: argument dependent lookup, friend functions
+// CRTP: curiously recurring template pattern
+
+#include <iostream>
+#include <optional>
+#include <vector>
+
+using namespace std;
+
+
+int main() {
+
+    return 0;
+}
